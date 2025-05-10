f8079840be034cbc456349850ba03a50ab75e769
MsAbigailS <msabigailrsmith@gmail.com>
2025-05-10 17:37:04 -0500

    update: call to action with animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ export default function Home() {
             {/* Hero/initial view */}
             <div className={`h-screen flex flex-col overflow-hidden`}>
                 <Header />
-                <div data-id="hero" className="flex flex-row justify-center items-center w-full flex-grow">
+                <div data-id="hero" className="flex lg:flex-grow lg:flex-row flex-col justify-center items-center w-full h-full">
                     <div className="flex flex-col justify-center items-center">
                         <div className="text-9xl font-inknut p-6">Abigail</div>
                         <div className="font-assistant text-2xl pb-6">Welcome to my website!</div>
@@ -36,23 +36,18 @@ export default function Home() {
             </div>
 
             {/* About me */}
-            <div data-id="about" className={`flex flex-row justify-center items-center pl-10 pr-10`}>
+            <div data-id="about" className={`flex lg:flex-row flex-col-reverse justify-center items-center pl-10 pr-10`}>
                 <div className={`flex-[1]`}>
                     img
                 </div>
-                <div className={`flex-[1]`}>
-                    <div id="professionalSummary" className={``}>
+                <div className={`flex-[1] lg:text-start text-center`}>
+                    <div id="professionalSummary">
                         <p data-id="subject-header">Software Engineer</p>
                         <p>I'm a <span data-id="highlight">software engineer</span> with a passion for building projects. I love to learn new things and work on challenging projects.</p>
                     </div>
                     <div id="skillsSummary">
                         <p data-id="subject-header">What am I good at?</p>
                         <p>I strongly believe software engineers should be able to work on <span data-id="highlight">any part of a project</span>, but I have a few areas of expertise:
-                            <ul>
-                                <li>My list item 1</li>
-                                <li>My list item 2</li>
-                                <li>My list item 3</li>
-                            </ul>
                         </p>
                     </div>
                     <div id="personalitySummary">
@@ -64,10 +59,34 @@ export default function Home() {
                 </div>
             </div>
 
-            <div data-id="subject-header" id="callToAction" className={`relative min-w-full overflow-hidden flex flex-col items-center mt-20`}>
-                {/* TODO: make hover invert color to highlight and change cursor */}
-                <p className={`border-2 min-w-full text-center`}>Curious about my projects?</p>
+
+
+            {/* call to action */}
+            <div className={`mt-25 flex justify-center items-center`}>
+                <div data-id="subject-header" id="callToAction" className={`relative rounded-xs overflow-hidden flex flex-col justify-center items-center hover:cursor-pointer group text-7xl`}>
+                    {/* background color movement */}
+                    <div className={`border-1 border-amber-500 min-h-full translate-z-0 flex-grow justify-center items-center flex 
+                        text-white
+                        p-2
+                        bg-gradient-to-r from-white to-white
+                        translate-x-[-102%]
+                        transition-all
+                        group-hover:translate-x-[0%]
+                        duration-[700ms] ease-in-out
+                        `}>
+                        Want to see my projects?
+                    </div>
+                    {/* text that shows */}
+
+                    <div className={`absolute mix-blend-difference 
+                        duration-[700ms] ease-in-out`}>
+                        Want to see my projects?
+                    </div>
+
+                </div>
             </div>
+
+
 
             {/* <div className={`flex flex-row`}>
                 <Card title="Project 1" description="A project I worked on!" technologies={["React", "Typescript", "Tailwindcss"]} theme="pink" />

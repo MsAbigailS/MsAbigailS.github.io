94852e2f02e8f460cb041f1e27c1186a4d5bb01a
MsAbigailS <msabigailrsmith@gmail.com>
2025-06-03 18:59:30 -0500

    update: download small screen

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -29,6 +29,18 @@ export default function Resume() {
         <div data-id="main" className="animate-fade-in animation-delay-300 h-screen bg-linear-30 from-[#468186]/10 to-blue-500/10">
             <Header left={<span onClick={goToHome}>Home</span>} right={<span onClick={goToProjects}>Projects</span>} />
             <div
+                className={`flex justify-center items-center`}
+            >
+                <a
+                    href={resume}
+                    download="Abby-Smith-Resume.pdf"
+                    className="lg:hidden border-2 m-3 p-3 rounded-lg inline-block text-white hover:bg-blue-700 transition-colors"
+                >
+                    Download Resume
+                </a>
+            </div>
+
+            <div
                 className="h-full bg-white"
             >
                 <embed className={`bg-white`} src={resume} type="application/pdf" width="100%" height="100%" />

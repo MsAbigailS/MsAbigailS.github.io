2d3cf4fab815462b083908c438f08af8a3180385
MsAbigailS <msabigailrsmith@gmail.com>
2025-06-03 18:45:39 -0500

    fix: bg for phone

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -16,7 +16,7 @@ export default function Resume() {
             <div
                 className="h-full"
             >
-                <embed src={resume} type="application/pdf" width="100%" height="100%" />
+                <embed className={`bg-white`} src={resume} type="application/pdf" width="100%" height="100%" />
             </div>
         </div >
     )
